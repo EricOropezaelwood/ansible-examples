482fecd4decb39282839e92a3be43a9c91cc7b9e
EricOropezaelwood <eric.oropezaelwood@gmail.com>
2021-07-28 16:13:16 -0500

    initial commit, one local example

---
+++ install-ansible.sh
@@ -0,0 +1,8 @@
+
+# create Python virtual environment
+python3 -m virtualenv ansible-venv
+# source the created virtual environment
+source ansible-venv/bin/activate
+# install ansible into the Python virtual environment
+python -m pip install ansible
+
